0e2032c7226c54bae445c40ffcd144cba92f989f
mg0009 <bdbdhdbgdg@gmail.com>
2026-04-06 01:58:33 +0530

    server.js

--- server.js
+++ server.js
@@ -2133,7 +2133,7 @@ app.post("/upload", (req, res) => {
 
 /* ================= USERS ================= */
 
-app.get("/users", (req, res) => {
+
   const logs = readLogs();
   if (!logs.length) return res.send("No data");
   res.send(renderUsersPage(logs));
